a8575c38cb493ae9b52772f76b6ec5cf0c19b05d
anm336 <83227183+anm336@users.noreply.github.com>
2023-03-08 00:17:47 +0530

    Time: 3 ms (96.21%), Space: 8.6 MB (24.13%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -3,19 +3,13 @@ public:
     vector<int> countBits(int n) {
         vector<int> ans;
         if(n==0) return {0};
-        if(n==1) return {0,1};
-        if(n==2) return {0,1,1};
         ans.push_back(0);
-        ans.push_back(1);
-        ans.push_back(1);
-        int power = 2;
-        for(int i=3;i<=n;i++){
-            if(i<pow(2, power)){
-                ans.push_back(ans[i-pow(2, power-1)] + ans[pow(2, power-1)]);
+        for(int i=1;i<=n;i++){
+            if(i%2 == 0){
+                ans.push_back(ans[i/2]);
             }
             else{
-                ans.push_back(1);
-                power++;
+                ans.push_back(ans[i-1] + 1);
             }
         }
         return ans;

fc463521e26ba48f55c42b1d26e488f89d0e79a7
anm336 <83227183+anm336@users.noreply.github.com>
2023-03-13 21:44:42 +0530

    Time: 258 ms (16.04%), Space: 13.5 MB (39.56%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -13,23 +13,24 @@ public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if(lists.size()==0) return NULL;
         if(lists.size()==1) return lists[0];
-        map<int,int> mapping;
-        for(int i=0;i<lists.size();i++){
-            ListNode* temp = lists[i];
-            while(temp!=NULL){
-                mapping[temp-> val]++;
-                temp = temp-> next;
-            }
+        while(lists.size()>1){
+            lists.push_back(mergeTwo(lists[0], lists[1]));
+            lists.erase(lists.begin());
+            lists.erase(lists.begin());
         }
-        ListNode* ans = new ListNode(1);
-        ListNode* ans2 = ans;
-        for(auto [x,y] : mapping){
-            for(int i=0;i<y;i++){
-                ListNode* temp = new ListNode(x);
-                ans-> next = temp;
-                ans = ans-> next;
-            }
+        return lists.front();
+    }
+    
+    ListNode* mergeTwo(ListNode* l1, ListNode* l2){
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        if(l1->val <= l2->val){
+            l1->next = mergeTwo(l1->next, l2);
+            return l1;
         }
-        return ans2-> next;
+        else{
+            l2->next = mergeTwo(l1, l2->next);
+            return l2;
+        }
     }
 };

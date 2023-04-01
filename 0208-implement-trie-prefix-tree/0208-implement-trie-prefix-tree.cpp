9556757c2872abf0714d606052ebfe74f66be2f5
anm336 <83227183+anm336@users.noreply.github.com>
2023-04-01 16:20:09 +0530

    Time: 60 ms (69.55%), Space: 45.1 MB (51.60%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,56 +1,75 @@
+struct Node{
+    Node* links[26];
+    //26 further nodes from this point for each alphabet
+    bool flag = false;
+    //flag to determine whehther thid node is end node
+    
+    bool containsKey(char ch){
+        return links[ch - 'a'] != NULL;
+    }
+    
+    void put(char ch, Node* node){
+        links[ch - 'a'] = node;
+    }
+    
+    Node* getNext(char ch){
+        return links[ch - 'a'];
+    }
+    
+    void setEnd(){
+        flag = true;
+    }   
+};
+
 class Trie {
+private: Node* root;
 public:
-    bool is_leaf;
-    Trie* children[26];
-    /** Initialize your data structure here. */
     Trie() {
-        this->is_leaf = false;
-        for(int i=0;i<26;i++)
-            this->children[i] = NULL;
+        root = new Node();
     }
     
-    /** Inserts a word into the trie. */
-    void insert(string word) {
-        Trie* root = this;
-        for (int i=0;i<word.length();i++)
-        {
-            int index = word[i] - 'a';
-            if (root->children[index] == NULL)
-                root->children[index] = new Trie();
-            root = root->children[index];
+    void insert(string words) {
+        Node* node = root;
+        for(int i=0;i<words.size();i++){
+            if(!node-> containsKey(words[i])){
+                node -> put(words[i], new Node());
+            }
+            //moves to reference trie
+            node = node-> getNext(words[i]);
         }
-        root->is_leaf = true;
+        //set end of word
+        node -> setEnd();
     }
     
-    /** Returns if the word is in the trie. */
     bool search(string word) {
-        if (this == NULL)
-            return false;
-        Trie* root = this;
-        for(int i=0;i<word.length();i++)
-        {
-            int index = word[i] - 'a';
-            root = root->children[index];
-            if (root == NULL)
+        Node* node = root;
+        for(int i=0;i<word.size();i++){
+            if(!node-> containsKey(word[i])){
                 return false;
+                break;
+            }
+            else node = node-> getNext(word[i]); 
         }
-        if (root->is_leaf)
-            return true;
-        return false;
+        return node-> flag == true;
     }
     
-    /** Returns if there is any word in the trie that starts with the given prefix. */
     bool startsWith(string prefix) {
-        if (this == NULL)
-            return false;
-        Trie* root = this;
-        for(int i=0;i<prefix.length();i++)
-        {
-            int index = prefix[i] - 'a';
-            root = root->children[index];
-            if (root == NULL)
+        Node* node = root;
+        for(int i=0;i<prefix.size();i++){
+            if(!node-> containsKey(prefix[i])){
                 return false;
+                break;
+            }
+            else node = node-> getNext(prefix[i]); 
         }
         return true;
     }
 };
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */

6b868bc18ea1e88e6f8db2847ee5cfb8ae6b4253
anm336 <83227183+anm336@users.noreply.github.com>
2023-04-13 10:25:26 +0530

    Time: 10 ms (57.01%), Space: 16.7 MB (5.37%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int n = pushed.size();
+        unordered_map<int,int> numoccur;
+        stack<int> stck;
+        int j=0;
+        for(int i=0;i<n;i++){
+            stck.push(pushed[i]);
+            numoccur[pushed[i]]++;
+            while(stck.size()>0 && stck.top() == popped[j]){
+                stck.pop();
+                j++;
+            }
+        }
+        if(stck.empty()) return true;
+        else return false;
+    }
+};

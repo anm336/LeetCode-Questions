7a6f342e72b62d0863ce7ffeecdafd7bb7262423
anm336 <83227183+anm336@users.noreply.github.com>
2023-04-24 09:37:57 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (41.71%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int s = stones.size();
+        if(s==1) return stones[0];
+        priority_queue<int> pq;
+        for(int i=0;i<s;i++){
+            pq.push(stones[i]);
+        }
+        while(pq.size()>1){
+            int y = pq.top();
+            pq.pop();
+            int x = pq.top();
+            pq.pop();
+            if(x!=y) pq.push(y-x); 
+        }
+        if(pq.size() == 0) return 0;
+        return pq.top();
+    }
+};

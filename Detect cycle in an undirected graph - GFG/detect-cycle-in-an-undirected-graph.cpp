950f2e45ad53b4fbfb503d700903b8ca1b27f332
anm336 <83227183+anm336@users.noreply.github.com>
2023-05-24 14:12:11 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -7,31 +7,59 @@ class Solution {
   public:
     // Function to detect cycle in an undirected graph.
     
-    bool dfs(int V, int par, vector<int> adj[], vector<bool> &vis){
+    // bool dfs(int V, int par, vector<int> adj[], vector<bool> &vis){
+    //     vis[V] = true;
+        
+    //     bool isLoop = false;
+        
+    //     for(int i=0;i<adj[V].size();i++){
+    //         if(vis[adj[V][i]] && adj[V][i] == par) continue;
+    //         if(vis[adj[V][i]]) return true;
+    //         isLoop |= dfs(adj[V][i], V, adj, vis);
+    //     }
+        
+    //     return isLoop;
+    // }
+    
+    bool bfs(int V, vector<int> adj[], vector<bool> &vis){
+        queue<int> q;
+        unordered_map<int,int> par;
+        par[V] = -1;
         vis[V] = true;
+        q.push(V);
         
-        bool isLoop = false;
-        
-        for(int i=0;i<adj[V].size();i++){
-            if(vis[adj[V][i]] && adj[V][i] == par) continue;
-            if(vis[adj[V][i]]) return true;
-            isLoop |= dfs(adj[V][i], V, adj, vis);
+        while(!q.empty()){
+            int x = q.front();
+            q.pop();
+            for(int i=0;i<adj[x].size();i++){
+                if(vis[adj[x][i]] && adj[x][i] != par[x]) return true;
+                else if(!vis[adj[x][i]]){
+                    q.push(adj[x][i]);
+                    vis[adj[x][i]] = true;
+                    par[adj[x][i]] = x;
+                }
+            }
         }
         
-        return isLoop;
+        return false;
     }
     
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
         vector<bool> vis(V, false);
+        // for(int i=0;i<V;i++){
+        //     if(vis[i]) continue;
+            
+        //     if(dfs(i, -1, adj, vis)){
+        //         return true;
+        //         break;
+        //     }
+        // }
         
         for(int i=0;i<V;i++){
             if(vis[i]) continue;
             
-            if(dfs(i, -1, adj, vis)){
-                return true;
-                break;
-            }
+            if(bfs(i, adj, vis)) return true;
         }
         
         return false;

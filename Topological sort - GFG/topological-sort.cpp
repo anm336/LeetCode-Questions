88bd9d7864d2202c3bdc333a396f37a4e7bbe02a
anm336 <83227183+anm336@users.noreply.github.com>
2023-05-25 15:09:38 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -7,33 +7,64 @@ class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order.
-	void dfs(int node, vector<int> adj[], stack<int> &stck, vector<bool> &vis){
-	    vis[node] = true;
+// 	void dfs(int node, vector<int> adj[], stack<int> &stck, vector<bool> &vis){
+// 	    vis[node] = true;
 	    
-	    for(auto neighbour: adj[node]){
-	        if(!vis[neighbour]) dfs(neighbour, adj, stck, vis);
-	    }
+// 	    for(auto neighbour: adj[node]){
+// 	        if(!vis[neighbour]) dfs(neighbour, adj, stck, vis);
+// 	    }
 	    
-	    stck.push(node);
-	}
+// 	    stck.push(node);
+// 	}
 	
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
-	    vector<int> ans;
-	    stack<int> stck;
-	    vector<bool> vis(V, false);
+	   // vector<int> ans;
+	   // stack<int> stck;
+	   // vector<bool> vis(V, false);
 	    
-	    for(int i=0;i<V;i++){
-	       if(!vis[i]) dfs(i, adj, stck, vis);
-	    }
+	   // for(int i=0;i<V;i++){
+	   //    if(!vis[i]) dfs(i, adj, stck, vis);
+	   // }
 	    
-	    while(!stck.empty()){
-	        ans.push_back(stck.top());
-	        stck.pop();
-	    }  
+	   // while(!stck.empty()){
+	   //     ans.push_back(stck.top());
+	   //     stck.pop();
+	   // }  
 	    
-	    return ans;
+	   // return ans;
+	   
+	   vector<int> ans;
+	   
+	   vector<int> indegree(V, 0);
+	   
+	   for(int i=0;i<V;i++){
+	       for(auto neighbour: adj[i]){
+	           indegree[neighbour]++;
+	       }
+	   }
+	   
+	   queue<int> q;
+	   for(int i=0; i<V; i++){
+	       if(indegree[i] == 0) q.push(i);
+	   }
+	   
+	   while(!q.empty()){
+	       int front = q.front();
+	       q.pop();
+	       
+	       ans.push_back(front);
+	       
+	       for(auto neighbour: adj[front]){
+	           indegree[neighbour]--;
+	           
+	           if(indegree[neighbour] == 0) q.push(neighbour);
+	       }
+	   }
+	   
+	   
+	   return ans;
 	}
 };
 

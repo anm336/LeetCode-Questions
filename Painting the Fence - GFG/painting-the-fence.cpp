702dc66b0fba9d2b8381625e142838fce82bda9d
anm336 <83227183+anm336@users.noreply.github.com>
2023-06-28 19:57:27 +0530

    Added solution - LeetHub

--- Painting the Fence - GFG/painting-the-fence.cpp
+++ Painting the Fence - GFG/painting-the-fence.cpp
@@ -10,22 +10,29 @@ using namespace std;
 
 class Solution{
     public:
-    long long func(int n, int k, vector<long long> &dp){
-        if(n==1) return k;
-        if(n==2){
-            long long val = (k*(k-1)%mod + k)%mod;
-            return val;
-        }
+    // long long func(int n, int k, vector<long long> &dp){
+    //     if(n==1) return k;
+    //     if(n==2){
+    //         long long val = (k*(k-1)%mod + k)%mod;
+    //         return val;
+    //     }
         
-        if(dp[n] != -1) return dp[n];
+    //     if(dp[n] != -1) return dp[n];
         
-        long long val = ((k-1)*func(n-2, k, dp)%mod) + ((k-1)*func(n-1, k, dp)%mod)%mod;
-        return dp[n] = val;
-    }
+    //     long long val = ((k-1)*func(n-2, k, dp)%mod) + ((k-1)*func(n-1, k, dp)%mod)%mod;
+    //     return dp[n] = val;
+    // }
     long long countWays(int n, int k){
         // code here
-        vector<long long> dp(n+1, -1);
-        return func(n, k, dp)%mod;
+        vector<long long> dp(n+1);
+        dp[1] = k;
+        dp[2] = (k*(k-1)%mod + k)%mod;
+        
+        for(int i=3;i<=n;i++){
+            dp[i] = (((k-1)*dp[i-2])%mod + ((k-1)*dp[i-1])%mod)%mod;
+        }
+        
+        return dp[n];
     }
 };
 

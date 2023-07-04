c98276331e28dd3caf7b9f5d35ea65bd82e1e2db
anm336 <83227183+anm336@users.noreply.github.com>
2023-07-04 09:25:31 +0530

    Time: 12 ms (35.14%), Space: 9.4 MB (98.52%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -2,15 +2,35 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        int ans;
-        unordered_map<int, int> m;
+        int ans = 0;
+        //Bit Mnipulation, using 32 bits
+        //For each of the numbers consider bit at a time from 0-32
+        //Now if for a position we get sum of 1 bits at a pos. multiple of 3, 
+        //Thta means the number occurs three times
+        //Else that bit 1 belongs to the answer
         
-        for(int i=0;i<n;i++){
-            m[nums[i]]++;
+        for(int i=0;i<32;i++){
+            int sum = 0;
+            
+            for(int j=0;j<nums.size();j++){
+                if((nums[j]>>i) & 1 == 1){
+                    sum++;
+                    sum%=3;
+                }
+            }
+            if(sum==1){
+                ans |= 1<<i;
+            }
         }
-        for(auto x: m){
-            if(x.second == 1) ans = x.first;
-        }
+        
+//         unordered_map<int, int> m;
+        
+//         for(int i=0;i<n;i++){
+//             m[nums[i]]++;
+//         }
+//         for(auto x: m){
+//             if(x.second == 1) ans = x.first;
+//         }
         
         return ans;
     }

34acf95fddef745792472e67437c5954ff738abd
anm336 <83227183+anm336@users.noreply.github.com>
2023-07-14 10:53:53 +0530

    Time: 165 ms (55.61%), Space: 54.7 MB (92.60%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
+//     int func(int i, int prev, vector<int> &arr, int diff, vector<vector<int>> &dp){
+//         if(i==arr.size()) return 0;
+        
+//         if(dp[i][prev+1] != -1) return dp[i][prev+1];
+//         int take = 0;
+//         if(prev==-1 || arr[i]-arr[prev] == diff){
+//             take = 1+func(i+1, i, arr, diff, dp);
+//         }
+//         int notake = func(i+1, prev, arr, diff, dp);
+        
+//         return dp[i][prev+1] = max(take, notake);
+//     }
+//     Gives TLE
     
     int longestSubsequence(vector<int>& arr, int difference) {
         int n = arr.size();
+        
+        unordered_map<int, int> m;
         int ans = 0;
-        unordered_map<int, int> mapping;
         
         for(int i=0;i<n;i++){
             int temp = arr[i] - difference;
-            int val = 0;
+            int toAdd = 0;
+            if(m.count(temp)) toAdd = m[temp];
             
-            if(mapping.count(temp)) val+= mapping[temp];
-            
-            mapping[arr[i]] = 1+val;
-            
-            ans = max(ans, 1+val);
+            m[arr[i]] = 1+toAdd;
+            ans = max(ans, m[arr[i]]);
         }
         
         return ans;
+        
+        // vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
+        // return func(0, -1, arr, difference, dp);
     }
 };

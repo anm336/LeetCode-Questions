6ce5f98c82fd03c01cfbd036959c8af40fa6bd33
anm336 <83227183+anm336@users.noreply.github.com>
2023-07-18 09:21:46 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
@@ -12,7 +12,9 @@ class Solution {
 		    
 		    for(int i=n-1;i>=0;i--){
 		        for(int j=n-1;j>=0;j--){
-		            if(str[i] == str[j] && i!=j) dp[i][j] = 1+dp[i+1][j+1];
+		            if(i!=j && str[i]==str[j]){
+		                dp[i][j] = 1+dp[i+1][j+1];
+		            }
 		            else dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
 		        }
 		    }

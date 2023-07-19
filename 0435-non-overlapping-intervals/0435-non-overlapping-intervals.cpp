a33847b8f50f5eb2b00671ade5641f5372be46f7
anm336 <83227183+anm336@users.noreply.github.com>
2023-07-19 21:31:43 +0530

    Time: 541 ms (24.94%), Space: 89.8 MB (58.83%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     static bool comp(vector<int> &a, vector<int> &b){
-        if(a[1]==b[1]) return a[0]<b[0];
+        //if(a[1]==b[1]) return a[0]<b[0];
         return a[1]<b[1];
     }
     
